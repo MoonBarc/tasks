a6db6c16e70dd80147cedcfe82d17c6d1500f859
MoonBarc <39464198+MoonBarc@users.noreply.github.com>
2021-05-16 11:11:07 -0400

    button design system

---
+++ panel/src/components/Button.svelte
@@ -0,0 +1,56 @@
+<script>
+    export let loading = false;
+    export let isDisabled = false;
+    export let type = "primary"
+    export let style = ""
+    let disabled = loading || isDisabled
+</script>
+
+<style>
+    button {
+        padding: 0.8rem 1.7rem;
+        min-width: 11rem;
+        border-radius: 6px;
+        /* border: black solid 1px; */
+        border: none;
+        text-transform: uppercase;
+        color: #FDFDFD;
+        font-size: 1.2rem;
+        background-color: #5292F1;
+        transition: filter 200ms;
+        cursor: pointer;
+    }
+
+    .primary {
+        background-color: #5292F1;
+    }
+
+    .primary.flat {
+        color: #5292F1;
+    }
+
+    .danger {
+        background-color: #F15252;
+    }
+
+    .danger.flat {
+        color:#F15252;
+    }
+
+    .secondary {
+        background-color: #4D6A95;
+    }
+
+    .secondary.flat {
+        color: #4D6A95;
+    }
+
+    .flat {
+        background: none;
+        color: #000;
+        border: solid 1px
+    }
+</style>
+
+
+<button style={style} class={type} disabled={disabled}><slot></slot></button>
--- panel/src/routes/index.svelte
+++ panel/src/routes/index.svelte
@@ -1,1 +1,8 @@
-<h1>Hello, world!</h1>
+<script>
+    import Button from "../components/Button.svelte";
+    let buttons = [ "primary", "secondary", "danger", "flat", "flat danger", "flat primary", "flat secondary"]
+</script>
+
+{#each buttons as type}
+    <Button style="margin-top: 1rem" type={type}>{type}</Button><br/>
+{/each}
